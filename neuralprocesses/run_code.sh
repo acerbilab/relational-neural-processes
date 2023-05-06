44ce2281e5fa7f0a09e46fcecadab77c14c8cbb4
Manuel Haussmann <manuel.haussmann@mailbox.org>
2023-05-06 16:35:49 +0300

    ignore pngs

--- neuralprocesses/run_code.sh
+++ neuralprocesses/run_code.sh
@@ -6,4 +6,5 @@ module load miniconda
 source activate rnp
 
 #srun -c 4 --time=00:15:00 --gres=gpu:1 python tmp.py
-srun -c 4 --time=00:15:00 --gres=gpu:1 python hyper.py
+srun -c 4 --time=01:30:00 --gres=gpu:1 python hyper.py
+# srun -c 4 --time=00:15:00  python hyper.py
