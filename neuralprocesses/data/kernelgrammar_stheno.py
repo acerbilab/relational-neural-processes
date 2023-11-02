050a3e3c1dd2926f01c533f976a6485566ab62ec
Manuel Haussmann <manuel.haussmann@mailbox.org>
2023-11-02 11:32:57 +0100

    Update kernelgrammar_stheno.py

--- neuralprocesses/data/kernelgrammar_stheno.py
+++ neuralprocesses/data/kernelgrammar_stheno.py
@@ -19,19 +19,6 @@ def generate_rbf():
     return st.EQ().stretch(sample_length())
 
 
-def generate_periodic():
-    raise NotImplementedError("See doc for a function on how to make kernels period")
-    kernel = gpy.kernels.PeriodicKernel()
-    kernel.lengthscale = th.exp(th.randn(1, 1))
-    kernel.period_length = th.exp(th.randn(1, 1))
-    return kernel
-
-
-def generate_white():
-    raise NotImplementedError("Implement manually")
-    return WhiteNoise()
-
-
 def generate_matern(nu):
     assert nu in [1 / 2, 3 / 2, 5 / 2]
     if nu == 1 / 2:
@@ -43,54 +30,15 @@ def generate_matern(nu):
     return kernel.stretch(sample_length())
 
 
-def generate_cosine():
-    raise NotImplementedError
-    kernel = gpy.kernels.CosineKernel()
-    # TODO: Simpson claims not to enforce a positivity constraint on the lengthscale
-    #   I don't get why that should work... and thus just take the absolute for now
-    kernel.period_length = thd.Cauchy(0, 5).sample((1, 1)).abs()
-    return kernel
-
-
-# class ModifiedLinear(gpy.kernels.Kernel):
-#     # Simpson et al. use K(x,z) = s^2 (x - c)(z - c) while gpy provides K(x,z) = s^2x * z
-#     is_stationary = False
-#     linear = gpy.kernels.LinearKernel()
-#     shift = nn.Parameter(thd.Cauchy(0, 5).sample((1, 1)))
-#     shift.requires_grad = False
-#
-#     def forward(self, x1, x2, **params):
-#         # TODO: Do this properly
-#         self.shift = self.shift.to(x1.device)
-#         self.linear = self.linear.to(x1.device)
-#
-#         x1_shift = x1 - self.shift
-#         x2_shift = x2 - self.shift
-#         return self.linear(x1_shift, x2_shift)
-
-
-# TODO: Not the full linear kernel from Simpson et al.
-def generate_linear():
-    return st.Linear().stretch(sample_length())
-
-
 def sample_basic_kernel(name=None, scale=False):
     if name == "EQ":
         kernel = generate_rbf()
-    elif name == "periodic":
-        kernel = generate_periodic()
-    elif name == "white":
-        kernel = generate_white()
     elif name == "matern12":
         kernel = generate_matern(1 / 2)
     elif name == "matern32":
         kernel = generate_matern(3 / 2)
     elif name == "matern52":
         kernel = generate_matern(5 / 2)
-    elif name == "cosine":
-        kernel = generate_cosine()
-    elif name == "linear":
-        kernel = generate_linear()
     else:
         raise NotImplementedError(f"{name} does not have a kernel implementation")
 
@@ -105,12 +53,9 @@ def get_names(single=False):
     basic = [
         "None",
         "EQ",
-        # "periodic",
         "matern12",
         "matern32",
         "matern52",
-        # "cosine",
-        # "linear", # TODO: Some device issues with stheano
     ]
     if single:
         # Don't return the None kernel
@@ -131,7 +76,6 @@ def get_names(single=False):
             continue
         else:
             names.append(list(pair))
-    # Simpson et al. get 34, I get 36. TODO: Figure out why
 
     return names
 
